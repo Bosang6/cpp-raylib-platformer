1b67e785ff7700e6bf7bfaf5cbcb0f14a2f0a1b7
TommasoRezzani <tommasorezzani@gmail.com>
2025-12-26 19:47:14 +0100

    Modified main by including logic and methods from Character and Playable_character

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,35 @@
 #include "raylib.h"
+#include "Character.h"
+#include "Playable_character.h"
 
 int main()
 {
     InitWindow(800, 450, "Hello raylib");
     SetTargetFPS(60);
 
+    // Crea il personaggio giocabile
+    PlayableCharacter player({400, 200}, {30, 50});
+
     while (!WindowShouldClose())
     {
+        float deltaTime = GetFrameTime();
+        
+        // Update
+        player.Update(deltaTime);
+        
+        // TODO: logica di collisione con piattaforme
+        // Es: if (CheckCollisionPlatforms(player.GetBounds())) {
+        //         player.OnLandOnPlatform();
+        //     }
+        
+        // Draw
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Hello raylib!", 190, 200, 20, DARKGRAY);
+            ClearBackground(RAYWHITE);
+            
+            player.Draw();
+            
+            DrawText("Use A/D or Arrow Keys to move, SPACE to jump", 10, 40, 20, DARKGRAY);
+            
         EndDrawing();
     }
 
